a468941d05db474ef92bbb499fdc2917e02832d6
FizzyApple12 <davideroeder@gmail.com>
2019-09-08 19:30:45 -0400

    Fix Inverted drive controls

--- src/main/java/frc/robot/commands/drive/Drive.java
+++ src/main/java/frc/robot/commands/drive/Drive.java
@@ -3,6 +3,7 @@ package frc.robot.commands.drive;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
+import frc.robot.RobotMap;
 import frc.robot.subsystems.Drivetrain;
 
 /**
@@ -28,15 +29,15 @@ public class Drive extends Command {
     protected void execute() {
         if (Robot.prefs.getBoolean("arcade", false)) {
             if (Robot.prefs.getBoolean("xbox", false)) {
-                Robot.driveTrain.arcadeDrive(-Robot.oi.xbox.getY(Hand.kLeft), Robot.oi.xbox.getX(Hand.kLeft), true);
+                Robot.driveTrain.arcadeDrive(-Robot.oi.xbox.getY(Hand.kLeft) * Robot.prefs.getDouble("xbox_sensitivity", 1.0d), Robot.oi.xbox.getX(Hand.kLeft) * Robot.prefs.getDouble("xbox_sensitivity", 1.0d), true);
             } else {
                 Robot.driveTrain.arcadeDrive(-Robot.oi.getFlight_one().getY(), Robot.oi.getFlight_one().getX(), true);
             }
         } else {
             if (Robot.prefs.getBoolean("xbox", false)) {
-                Robot.driveTrain.TankDrive(-Robot.oi.xbox.getY(Hand.kLeft), Robot.oi.xbox.getY(Hand.kRight));
+                Robot.driveTrain.TankDrive(-Robot.oi.xbox.getY(Hand.kLeft) * Robot.prefs.getDouble("xbox_sensitivity", 1.0d), -Robot.oi.xbox.getY(Hand.kRight) * Robot.prefs.getDouble("xbox_sensitivity", 1.0d));
             } else {
-                Robot.driveTrain.TankDrive(-Robot.oi.getFlight_one().getY(), Robot.oi.getFlight_two().getY());
+                Robot.driveTrain.TankDrive(-Robot.oi.getFlight_one().getY(), -Robot.oi.getFlight_two().getY());
             }
         }
     }
